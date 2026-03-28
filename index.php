c2318ccbdfa25b52563dfe9e4371284fceb906b8
Zenettany-dev <88mm5kvb4s@privaterelay.appleid.com>
2026-03-28 17:19:07 +0300

    Create PHP Entry Point

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// Entry point for the application
+
+// Include necessary files
+// You can structure your application better with namespaces and class autoloading
+
+// Define constants for JSON database and other configurations
+define('DB_FILE', __DIR__ . '/data/database.json');
+
+// Other application setup code fast here
+
+?>
